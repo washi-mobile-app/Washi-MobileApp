6a1ef61a259bdc2344f4147a0bfcced51e071f79
Akirakii <akiraki.awa@gmail.com>
2021-05-05 16:42:28 -0500

    first commit

---
+++ app/src/main/java/com/example/prueba/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.prueba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        ga();
+    }
+
+    private fun ga() {
+        TODO("Not yet implemented")
+    }
+}
