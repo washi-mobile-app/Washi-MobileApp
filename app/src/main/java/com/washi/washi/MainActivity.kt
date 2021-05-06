ea71f6bc123b3023884538dbc44a69a9374dc924
Akirakii <akiraki.awa@gmail.com>
2021-05-05 20:24:47 -0500

    minor fixes into res and some implementation on searchLaundriesActivity

--- app/src/main/java/com/washi/washi/MainActivity.kt
+++ app/src/main/java/com/washi/washi/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.washi.washi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.washi.washi.entities.Laundry
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val REQUEST_CODE = 1
     var laundries = ArrayList<Laundry>()
+    var laundriesToShow = ArrayList<Laundry>()
     var laundryAdapter = LaundryAdapter(laundries)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +32,7 @@ class MainActivity : AppCompatActivity() {
         laundries.add(Laundry("Lavanderia Cledmir", "Av. faucett", 10f))
         laundries.add(Laundry("Aqui lavamos bien", "Av. La marina", 8f))
 
+        laundriesToShow = laundries
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -36,4 +41,17 @@ class MainActivity : AppCompatActivity() {
 
         return true
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.itemSearch -> {
+                val intent = Intent(this, SearchLaundriesActivity::class.java)
+                startActivityForResult(intent, REQUEST_CODE)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
 }
--- app/src/main/java/com/washi/washi/SearchLaundriesActivity.kt
+++ app/src/main/java/com/washi/washi/SearchLaundriesActivity.kt
@@ -2,10 +2,21 @@ package com.washi.washi
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_search_laundries.*
 
 class SearchLaundriesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_laundries)
+
+        btSearchLaundries.setOnClickListener {
+            searchLaundries()
+        }
+    }
+
+    private fun searchLaundries() {
+        val name = etLaundryName.text.toString()
+        val address = etLaundryAddress.text.toString()
+        val price = etLaundryPrice.text.toString()
     }
 }
