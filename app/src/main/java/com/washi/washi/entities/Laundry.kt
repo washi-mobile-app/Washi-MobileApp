df506b399632ca08c37521a28b4529f15a787221
cledmir <56932765+cledmir@users.noreply.github.com>
2021-05-05 20:08:04 -0500

    add val to entity

--- app/src/main/java/com/washi/washi/entities/Laundry.kt
+++ app/src/main/java/com/washi/washi/entities/Laundry.kt
@@ -3,5 +3,8 @@ package com.washi.washi.entities
 class Laundry (
         val name: String?,
         val address: String?,
-        val price: Float?
+        val price: Float?,
+        val email: String?,
+        val district: String?,
+        val phone: Long?
         )
