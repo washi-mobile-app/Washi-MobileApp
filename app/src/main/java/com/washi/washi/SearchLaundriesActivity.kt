b48ad0ccbe4ca4c5970728df4ff00c6e7c8baefb
Akirakii <akiraki.awa@gmail.com>
2021-05-05 19:25:44 -0500

    string extraction and activity_search_laundries.xml implemented

---
+++ app/src/main/java/com/washi/washi/SearchLaundriesActivity.kt
@@ -0,0 +1,11 @@
+package com.washi.washi
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SearchLaundriesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_laundries)
+    }
+}
