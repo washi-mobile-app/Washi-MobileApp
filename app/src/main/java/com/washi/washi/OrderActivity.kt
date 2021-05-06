424d6e93889f7c5628e35b4f3f3d35c64870ed04
cledmir <56932765+cledmir@users.noreply.github.com>
2021-05-05 22:29:20 -0500

    create activity

---
+++ app/src/main/java/com/washi/washi/OrderActivity.kt
@@ -0,0 +1,12 @@
+package com.washi.washi
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_search_laundries.*
+
+class OrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_laundries)
+    }
+}
